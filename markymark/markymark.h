a54821d0188c9b2213102dc5c9fbcfd31c3b08cf
Thomas Gmelig Meyling <thomas@youngmediaexperts.nl>
2020-11-05 14:35:05 +0100

    Add project with Carthage framework.

---
+++ markymark/markymark.h
@@ -0,0 +1,18 @@
+//
+//  markymark.h
+//  markymark
+//
+//  Created by Thomas Gmelig Meyling on 05/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for markymark.
+FOUNDATION_EXPORT double markymarkVersionNumber;
+
+//! Project version string for markymark.
+FOUNDATION_EXPORT const unsigned char markymarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <markymark/PublicHeader.h>
+
+
